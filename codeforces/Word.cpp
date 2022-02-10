9426433de9f6c4ec3b1303f77b59000d44706c9e
mohittk <mvk1407@gmail.com>
2022-02-10 17:53:27 +0530

    day2

---
+++ codeforces/Word.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str; cin >>str;
+    int up=0, low=0;
+
+    for(int i = 0; i <str.size(); i++){
+        if(str[i] >= 'A' && str[i] <= 'Z'){
+            up++;
+        }
+        if(str[i] >= 'a' && str[i] <='z'){
+            low++;
+        }
+
+        
+    }
+    if(up>low){
+        for(int i=0; i<str.size(); i++){
+            char ch = str[i];
+            cout<<toupper(ch);
+            
+        }
+    }
+    if(low>up || low==up){
+        for(int i=0; i<str.size(); i++){
+            char ch = str[i];
+            cout<<tolower(ch);
+            
+        }
+
+    }
+    
+}
